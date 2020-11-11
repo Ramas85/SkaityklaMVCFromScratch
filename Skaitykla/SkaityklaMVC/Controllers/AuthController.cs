688816501e3630e0266be67711b6890c0584c612
Ramunas <ramasit85@gmail.com>
2020-11-11 11:13:45 +0200

    Add: LoginModel,AuthController,Admin panel

--- Skaitykla/SkaityklaMVC/Controllers/AuthController.cs
+++ Skaitykla/SkaityklaMVC/Controllers/AuthController.cs
@@ -16,9 +16,9 @@ namespace SkaityklaMVC.Controllers
             return View();
         }
         //Kas tas HttpPost? Kai susikurem AuthController ir susikurem Index View,Index View'e padarem registracijos forma.
-        //Butent is tos formos, kai useris paspaus submit per metoda "post" ateis duomenis per post metoda cia yra filtras httpPost.
+        //Butent is tos formos, kai useris paspaus submit btn, per metoda "post" ateis duomenis.Juos patikrins ar tikrai atejo per post metoda (httpPost filtras).
         [HttpPost]
-        //sumepinam i duomenu modeli ir siunciam i 
+        //sumepinam i duomenu modeli ir patikrinam ar jis yra validus.
         public IActionResult Login(LoginViewModel model)
         {
             if (!ModelState.IsValid)
