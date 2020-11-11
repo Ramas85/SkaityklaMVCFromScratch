19c66229323941f6477a66f1bdd03352bb034401
Ramunas <ramasit85@gmail.com>
2020-11-11 11:11:24 +0200

    Add: LoginModel,AuthController,Admin panel

---
+++ Skaitykla/SkaityklaMVC/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkaityklaMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace SkaityklaMVC.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //Kas tas HttpPost? Kai susikurem AuthController ir susikurem Index View,Index View'e padarem registracijos forma.
+        //Butent is tos formos, kai useris paspaus submit per metoda "post" ateis duomenis per post metoda cia yra filtras httpPost.
+        [HttpPost]
+        //sumepinam i duomenu modeli ir siunciam i 
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.AuthError = "Blogi prisijungimo duomenys";
+                return View("~/Views/Auth/Index.cshtml");
+            }
+            return View();
+        }
+        public IActionResult Admin()
+        {
+            return View();
+        }
+    }
+}
---
+++ Skaitykla/SkaityklaMVC/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Routing.Template;
+using Microsoft.Extensions.DependencyInjection;
+
+
+// Paskaita 8. 2020-10-15 Skaitykla nuo nulio.
+
+
+namespace SkaityklaMVC
+{
+    public class Startup
+    {
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+            {
+             routes.MapRoute(name:"default", template:"{controller=Home}/{action=Index}/{id?}");       
+            });           
+        }
+    }
+}
